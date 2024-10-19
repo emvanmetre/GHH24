d6ec8f407b081d96a67b12f571c5ab433ee71d08
BGV <26331505+bgv2@users.noreply.github.com>
2024-10-19 19:37:47 -0400

    almost working

--- src/lookup.js
+++ src/lookup.js
@@ -1,38 +1,45 @@
-import {add} from 'date-fns';
+import { add } from 'date-fns';
 import expDates from './expDates.json';
+expDates = Object.freeze(expDates);
 function addDuration(durationArray) {
-    if (!Array.isArray(durationArray) || durationArray.length !== 2) {
-      throw new Error('Invalid duration array');
-    }
-    const [amount, unit] = durationArray;
-    const duration = { [unit]: parseInt(amount, 10) };
-    return add(new Date(), duration);
+  if (!Array.isArray(durationArray) || durationArray.length !== 2) {
+    throw new Error('Invalid duration array: ' + durationArray);
+  }
+  const [amount, unit] = durationArray;
+  const duration = { [unit]: parseInt(amount, 10) };
+  return add(new Date(), duration);
+}
+
+const findExpireDates = (foodName) => {
+  const foodData = expDates[foodName.toLowerCase().trim()];
+  console.log(expDates[foodName.toLowerCase().trim()]);
+  if (!foodData) {
+    return null;
   }
 
-  const findExpireDates = (foodName) => {
-    const foodData = expDates[foodName.toLowerCase().trim()];
-    if (!foodData) {
-      return null;
-    }
+  console.log(foodData);
 
-    if (foodData.refrigerated) {
-      foodData.refrigerated = addDuration(foodData.refrigerated);
-    }
-
-    if (foodData.frozen) {
-      foodData.frozen = addDuration(foodData.frozen);
-    }
-
-    if (foodData.unrefrigerated) {
-      foodData.unrefrigerated = addDuration(foodData.unrefrigerated);
-    }
-
-    return {
-      name: foodName.toLowerCase().trim(),
-      category: foodData.category,
-      refrigerated: foodData.refrigerated,
-      frozen: foodData.frozen,
-      unrefrigerated: foodData.unrefrigerated,
-    }
+  if (foodData.refrigerated && foodData.refrigerated !== "inf") {
+    console.log("refrigerated", foodData.refrigerated);
+    foodData.refrigerated = addDuration(foodData.refrigerated);
   }
+
+  if (foodData.frozen && foodData.frozen !== "inf") {
+    console.log("frozen", foodData.frozen);
+    foodData.frozen = addDuration(foodData.frozen);
+  }
+
+  if (foodData.unrefrigerated && foodData.unrefrigerated !== "inf") {
+    console.log("unrefrigerated", foodData.unrefrigerated);
+    foodData.unrefrigerated = addDuration(foodData.unrefrigerated);
+  }
+
+  return {
+    name: foodName.toLowerCase().trim(),
+    category: foodData.category,
+    refrigerated: foodData.refrigerated,
+    frozen: foodData.frozen,
+    unrefrigerated: foodData.unrefrigerated,
+  }
+}
 export default findExpireDates;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { ButtonIcon, Navbar, AutoCompleteComponent } from "../components";
+import { Button, Modal, Space } from 'antd';
+import findExpireDates from "../lookup"
 import expDates from "../expDates.json";
+import { formatDistanceToNow } from "date-fns";
 
 function Home() {
   const autocompleteValues = Object.keys(expDates).map((name) => {
     return { value: name };
   });
 
-  const [savedFoods, setSavedFoods] = React.useState<string[]>([]);
+  const [savedFoods, setSavedFoods] = React.useState<any[]>([]);
   const handleSelect = (value: string) => {
-    setSavedFoods([...savedFoods, value]);
+    const d = findExpireDates(value);
+    if (!d) {
+      prompt("Sorry, we don't have information on that food. Please try again.");
+      return;
+    }
+    console.log(d)
+    const validOpts = []
+    if (d.frozen) validOpts.push("frozen");
+    if (d.refrigerated) validOpts.push("refrigerated");
+    if (d.unrefrigerated) validOpts.push("unrefrigerated");
+    const opt: keyof typeof d = (prompt("Where is your food stored? " + validOpts.join(", ")) || "unrefrigerated") as keyof typeof d;
+    if (!validOpts.includes(opt)) {
+      prompt("Invalid option. Please try again.");
+      return;
+    }
+    const date = d[opt];
+    setSavedFoods([...savedFoods, { name: value, date }]);
   }
 
     return (
@@ -20,8 +39,13 @@ function Home() {
         <AutoCompleteComponent options={autocompleteValues} handleSelect={handleSelect}></AutoCompleteComponent>
         <h1>Saved Foods</h1>
         <ul>
-          {savedFoods.map((food) => (
-            <li key={food}>{food}</li>
+          {savedFoods
+          .slice()
+          .sort((a, b) => a.date - b.date)
+          .map(({name, date}) => (
+            <li key={name}>{name}: {
+              date === "inf" ? "♾" : formatDistanceToNow(date, { addSuffix: true })
+            }</li>
           ))}
         </ul>
         <ButtonIcon icon="checkmark">Hello there!</ButtonIcon>
