3dc179980c6ac82805a26b5537dad9b81f2c47c5
emvanmetre <emv4ew@virginia.edu>
2024-10-19 14:49:55 -0400

    feat: create navbar

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import { ButtonIcon } from './button'
 import Icons from './icons.tsx'
+import Navbar from './navbar.tsx'
 
-export {ButtonIcon, Icons}
+export {ButtonIcon, Icons, Navbar}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { ButtonIcon } from "../components";
+import { ButtonIcon, Navbar } from "../components";
 
 function Home() {
     return (
       <div className="Homepage">
+        <Navbar></Navbar>
         <ButtonIcon icon="checkmark">Hello there!</ButtonIcon>
       </div>
     );
